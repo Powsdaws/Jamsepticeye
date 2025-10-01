729c467066f3966cf03fa1e705a5eaaca82b8b8d
Marius Holm <marho@itu.dk>
2025-10-01 22:08:49 +0200

    added menu

---
+++ Death_is_an_opportunity/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Called by Start Button
+    public void PlayGame()
+    {
+        // Loads the next scene in Build Settings
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    // Called by Quit Button
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game!"); // Works in Editor
+        Application.Quit();      // Works in Build
+    }
+}
